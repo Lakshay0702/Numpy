cd66d7fcd5eb5cd4f6f48afadc8e9c1bb26b49fb
Lakshay0702 <106376282+Lakshay0702@users.noreply.github.com>
2022-05-29 01:29:53 -0700

    Create Q30.py

---
+++ Q30.py
@@ -0,0 +1,6 @@
+#Write a NumPy program to test whether all elements in an array evaluate to True.
+import numpy as np
+print(np.all([[True,False],[True,True]]))
+print(np.all([[True,True],[True,True]]))
+print(np.all([10, 20, 0, -50]))
+print(np.all([10, 20, -50]))
