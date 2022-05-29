e2ef101fe51347589a21c710e00a1c25608a8490
Lakshay0702 <106376282+Lakshay0702@users.noreply.github.com>
2022-05-29 01:27:35 -0700

    Create q24.py

---
+++ q24.py
@@ -0,0 +1,9 @@
+#Write a NumPy program to convert an array to a float type.
+import numpy as np
+import numpy as np
+a = [1, 2, 3, 4]
+print("Original array")
+print(a)
+x = np.asfarray(a)
+print("Array converted to a float type:")
+print(x)
